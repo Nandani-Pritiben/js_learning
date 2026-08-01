d694b7ab703272cd01c81eb7257543f377fd5f34
NandaniPritiben <neha14112018@gmail.com>
2026-08-01 16:02:53 +0000

    comparision operator

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,4 @@
+// Primitive datatype Non primitive data type based how memory store that value , call by value and call by reference
+
+// Primitive - number,string,boolean,null,undefined,symbol
+// nonprimitive datatype- function,array,object
