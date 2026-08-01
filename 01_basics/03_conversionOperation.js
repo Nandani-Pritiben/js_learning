0b607eec3d4f9ee8f64d7052c30f490918510a7e
NandaniPritiben <neha14112018@gmail.com>
2026-08-01 14:59:49 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,31 @@ let stringSumNumber=String(sumNumber);
 
 console.log(typeof(stringSumNumber));
 console.log(stringSumNumber);
+
+//***********Operations************/
+let value = 3;
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2**2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+let s="neha"
+let s1=' khalas'
+let s3= s + s1;
+
+console.log(s3);
+
+str='1';
+str1=2;
+console.log(str + str1);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+//prefix 
+// postfix
