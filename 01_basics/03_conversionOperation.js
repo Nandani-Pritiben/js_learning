01807f658234d33f68e7a3ad36ac998a71262a23
NandaniPritiben <neha14112018@gmail.com>
2026-08-01 13:00:22 +0000

    Conversion of Data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="00abc";
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let isLogin=-1;
+
+let booleanIsLogin=Boolean(isLogin);
+
+console.log(booleanIsLogin);
+
+let sumNumber=99;
+
+let stringSumNumber=String(sumNumber);
+
+console.log(typeof(stringSumNumber));
+console.log(stringSumNumber);
