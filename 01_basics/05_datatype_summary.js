62b47ecf812ba3950aa4a50ba5600acb851c1ed2
NandaniPritiben <neha14112018@gmail.com>
2026-08-01 16:21:57 +0000

    memory type stack and heap

--- 01_basics/05_datatype_summary.js
+++ 01_basics/05_datatype_summary.js
@@ -2,3 +2,22 @@
 
 // Primitive - number,string,boolean,null,undefined,symbol
 // nonprimitive datatype- function,array,object
+
+//*******memory- Stack- primitive,heap-non-primitive */
+
+let myYoutubeName='neha';
+let anotherName=myYoutubeName;
+anotherName='nisha';
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let nirali={
+    email:'nirali@org.com',
+    upi:'nirali@ybl'
+}
+let harsh=nirali;
+harsh.email='niraliharsh@gmail.com';
+console.log(harsh);
+console.log(nirali);
+
