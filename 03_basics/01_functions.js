6085c31b27f8522953d6f6e705284411b7d174d7
NandaniPritiben <neha14112018@gmail.com>
2026-08-03 20:39:53 +0000

    function with array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,3 +43,36 @@ function addTwoNumbers(num1,num2){
     console.log(username);
     
 
+    function calculateCarPrice(val1,val2,...num1){
+      return num1;
+      
+    }
+
+    console.log(calculateCarPrice(2));
+    console.log(calculateCarPrice(2,8,9,4,5));
+    
+
+const user={
+    name:'hitesh',
+    price:199
+}
+
+function handleObject(anyobject){
+return `username is ${anyobject.name} and price is ${anyobject.price}`
+}
+
+// let r1=handleObject(user);
+let r1=handleObject({
+    name:'neha',
+    price:'free'
+})
+console.log(r1);
+
+const myNewArray=[1,2,3,4,5];
+
+function returnSecondValue(getArray){
+    console.log(getArray);
+    
+}
+returnSecondValue(myNewArray);
+returnSecondValue([2,4,6])
