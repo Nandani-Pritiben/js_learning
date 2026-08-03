70c2935475a6821e563778f0b10b20960784c99e
NandaniPritiben <neha14112018@gmail.com>
2026-08-03 15:24:14 +0000

    feat:object file basic

---
+++ 02_basics/03_Object.js
@@ -0,0 +1,60 @@
+//Singleton
+
+//Object Literals
+
+const mysym = Symbol("key1");
+
+const JSUser={
+    name:"nandani",
+    "full name":"khalas Nandani",
+    mysym:'mykey1',
+    // [mysym]:'mykey1',
+    [mysym]:Symbol('mykey2'),
+    age:18,
+    location:'ahmedabad',
+    email:'neha@org.com',
+    isLoggedIn:false,
+    LastLoginDays:['Saturday','Sunday']
+};
+console.log(JSUser);
+
+console.log(JSUser.email);
+console.log(JSUser['email']);
+
+// console.log(JSUser."full name"); //error throw
+
+console.log(JSUser["full name"]);
+
+console.log(typeof JSUser.mysym);
+
+console.log(typeof JSUser[mysym]);
+
+console.log(Object.getOwnPropertySymbols(JSUser));
+console.log(JSUser);
+
+JSUser.email='neha@google.com';
+console.log(JSUser.email);
+
+// Object.freeze(JSUser);
+
+JSUser.email='neha@js.com'
+console.log(JSUser.email);
+
+JSUser.greeting=function(){
+    console.log("Hello JS");//2
+    // return "Hello js" //1
+}
+
+// console.log(JSUser.greeting);//2
+
+JSUser.greeting();//2
+
+// console.log(JSUser.greeting()); // 1
+
+
+JSUser.greeTwo = function () {
+    console.log(`Hello ${this.LastLoginDays}`)
+}
+
+console.log(JSUser);
+console.log(JSUser.greeTwo());
